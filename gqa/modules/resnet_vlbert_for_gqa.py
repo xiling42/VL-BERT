45a4182c31420427896d7679feb6f1135841f5f3
xiling zhang <55151938+xiling42@users.noreply.github.com>
2021-04-18 18:54:04 -0400

    Update resnet_vlbert_for_gqa.py

--- gqa/modules/resnet_vlbert_for_gqa.py
+++ gqa/modules/resnet_vlbert_for_gqa.py
@@ -315,7 +315,7 @@ class ResNetVLBERT(Module):
 
         # loss
         criterion = nn.CrossEntropyLoss()
-        loss = criterion(logits, answers.unsqueeze(0))
+        loss = criterion(logits, answers.unsqueeze(1))
 
         outputs.update({'label_logits': logits,
                         'label': answers,
