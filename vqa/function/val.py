78fcb40e0727ed5f042eef8aa0d1e5a05488355b
xiling zhang <55151938+xiling42@users.noreply.github.com>
2021-04-19 22:02:20 -0400

    Update val.py

--- vqa/function/val.py
+++ vqa/function/val.py
@@ -7,7 +7,7 @@ from common.trainer import to_cuda
 def do_validation(net, val_loader, metrics, label_index_in_batch):
     net.eval()
     metrics.reset()
-    for nbatch, batch in enumerate(val_loader):
+    for batch in enumerate(val_loader):
         batch = to_cuda(batch)
         label = batch[label_index_in_batch]
         datas = [batch[i] for i in range(len(batch)) if i != label_index_in_batch % len(batch)]
